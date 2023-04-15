e21403d83d95180ed1e91964f6c96e069f65a901
yaginumatti <ryohei122yagi@gmail.com>
2023-04-15 16:06:43 +0900

    フォームの入力文字を消す

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
-const Form = ({ setCity, getWeather }) => {
+const Form = ({ city, setCity, getWeather }) => {
   return (
     <form onSubmit={getWeather}>
-      <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
+      <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} value={city}/>
       <button type="submit">Get Weather</button>
     </form>
   );

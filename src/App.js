6b42a963146014e7866f88d27efa27cec31e2e72
yaginumatti <ryohei122yagi@gmail.com>
2023-04-15 12:01:18 +0900

    Titleのインポート

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Title from './components/Title';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>こんにちは</h1>
+      <Title />
     </div>
   );
 }

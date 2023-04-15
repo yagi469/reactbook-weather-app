a51022b3f64bc2475e690f121ff130f86717d06a
yaginumatti <ryohei122yagi@gmail.com>
2023-04-15 16:02:01 +0900

    <>の利用

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,7 +1,7 @@
 const Results = ({ results }) => {
   const { country, cityName, temperature, conditionText, icon } = results;
   return (
-    <div>
+    <>
       {country && <div className="results-country">{country}</div>}
       {cityName && <div className="results-city">{cityName}</div>}
       {temperature && <div className="results-temp">{temperature}<span>℃</span></div>}
@@ -11,7 +11,7 @@ const Results = ({ results }) => {
           <span>{conditionText}</span>
         </div>
       }
-    </div>
+    </>
   );
 };
 

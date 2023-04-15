8e6f024e6099aed2e46c1b81a86ad713f1e0ac0e
yaginumatti <ryohei122yagi@gmail.com>
2023-04-15 15:49:08 +0900

    分割代入の適用

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,13 +1,14 @@
-const Results = (props) => {
+const Results = ({ results }) => {
+  const { country, cityName, temperature, conditionText, icon } = results;
   return (
     <div>
-      {props.results.country && <div className="results-country">{props.results.country}</div>}
-      {props.results.cityName && <div className="results-city">{props.results.cityName}</div>}
-      {props.results.temperature && <div className="results-temp">{props.results.temperature}<span>℃</span></div>}
-      {props.results.conditionText &&
+      {country && <div className="results-country">{country}</div>}
+      {cityName && <div className="results-city">{cityName}</div>}
+      {temperature && <div className="results-temp">{temperature}<span>℃</span></div>}
+      {conditionText &&
         <div className="results-condition">
-          <img src={props.results.icon} alt="icon"/>
-          <span>{props.results.conditionText}</span>
+          <img src={icon} alt="icon"/>
+          <span>{conditionText}</span>
         </div>
       }
     </div>
